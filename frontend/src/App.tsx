acce6e6a1980f9e262ce8720bf89af7f8e88c3b3
mayconndouglass <mayconn.douglass1414@gmail.com>
2024-12-15 22:10:09 -0300

    chore: add react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider } from 'react-router-dom'
+
+import { ThemeProvider } from '@/components/theme/theme-provider'
+
+import { queryClient } from './lib/react-query'
+import { router } from './routes'
+
 export function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ThemeProvider>
   )
 }
