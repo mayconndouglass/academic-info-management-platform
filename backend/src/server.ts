c5a8afb13562d49fc1891e79b1f083712e5af5e2
Maycon Douglas <mayconn.douglass1414@gmail.com>
2026-06-23 21:43:07 -0300

    chore

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,4 +4,4 @@ import { app } from "./app"
 
 const port = Number(process.env.PORT) || 3333
 
-app.listen({ port }, () => console.log("🚀 Server running"))
+app.listen({ port, host: "0.0.0.0" }, () => console.log("🚀 Server running"))
