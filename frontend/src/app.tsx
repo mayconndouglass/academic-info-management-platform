7b286facf53759615ea4de29cfff0ca8338da78d
Maycon Douglas <mayconn.douglass1414@gmail.com>
2026-06-23 22:41:16 -0300

    refactor: app.tsx

---
+++ frontend/src/app.tsx
@@ -0,0 +1,19 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider } from 'react-router-dom'
+
+import { ThemeProvider } from '@/components/theme/theme-provider'
+
+import { queryClient } from './lib/react-query'
+import { router } from './routes'
+
+export function App() {
+  return (
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <div className="flex min-h-screen flex-col">
+          <RouterProvider router={router} />
+        </div>
+      </QueryClientProvider>
+    </ThemeProvider>
+  )
+}
